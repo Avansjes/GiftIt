cebb2134e5b82449bd2808d9d1ae21b468f7b3ec
Robin Koele <robinkoele@eu.ute.com>
2019-12-13 09:53:39 +0100

    Bestanden samengevoegd
    en mijn gedeelte geupdate.

---
+++ SQL-GiftIt/Create-Stored Procedures.sql
@@ -0,0 +1,44 @@
+/* (Robin) Middels onderstaande procedure kan er een volgnummer worden aangemaakt voor een pakket. */
+/* Create Stored Procedure */
+DELIMITER //
+CREATE PROCEDURE `insert_pakketvolgnr` (
+	IN `par_TrackingNO` VARCHAR(40), 
+	IN `par_Vervoerder` VARCHAR(35), 
+	IN `par_ServiceLevel` VARCHAR(35), 
+	IN `par_OrderID` INT(6), 
+BEGIN 
+	INSERT INTO `pakketvolgnr` ( 
+	`TrackingNO`, 
+	`Vervoerder`, 
+	`ServiceLevel`, 
+	`OrderID`) VALUES ( 
+	NULL, 
+	par_TrackingNO, 
+	par_Vervoerder, 
+	par_ServiceLevel, 
+	par_OrderID);
+END;
+//
+
+/* CALL Stored Procedure */
+CALL `insert_pakketvolgnr` (TrackingNO,Vervoerder,ServiceLevel,OrderID)
+
+
+/* Stored Prcedure Vervoerder aanmaken */
+DELIMITER //
+CREATE PROCEDURE `insertVervoerder`
+  (IN `par_vervoerder` VARCHAR(35),IN `par_ServiceLevel` VARCHAR(35), OUT `resultaat1` VARCHAR(35),OUT `resultaat2` VARCHAR(35),OUT `resultaat3` VARCHAR(80),OUT `resultaat4` VARCHAR(50))
+BEGIN
+  INSERT INTO `giftit`.`vervoerder`(`Vervoerder`,`ServiceLevel`) 
+  VALUES(`par_vervoerder`, `par_ServiceLevel`);
+  Select `Vervoerder`,`ServiceLevel`,`Omschrijving`,`LinkVervoerder`
+  into `resultaat1`,`resultaat2`,`resultaat3`,`resultaat4`
+  from `giftit`.`vervoerder`
+  where `Vervoerder`= `par_vervoerder`
+  and `ServiceLevel` = `par_ServiceLevel`;
+END;
+//
+
+/* Voorbeeld nieuwe vervoerder toevoegen middels Storede Function */
+call `insertVervoerder` ('FEDEX','EXPRESS', @restultaat1,@restultaat2,@restultaat3,@restultaat4);
+select @restultaat1,@restultaat2,@restultaat3,@restultaat4;
---
+++ SQL-GiftIt/INSERT-Data.sql
@@ -0,0 +1,17 @@
+/* INSERT data `gebruikers` */
+INSERT INTO `gebruikers` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('124913', 'jan.jansen@gmail.com', 'Jantje', 'Jan', 'Jansen', 'Kerkstraat', '21', '1234DE', 'Schiedam', 'Nederland', 'Gebruiker');
+INSERT INTO `gebruikers` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('232143', 'familiedebie@hotmail.com', 'Jo de Bie', 'Jo', 'de Bie', 'Markt', '6', '3221AA', 'Amsterdam', 'Nederland', 'Gebruiker');
+INSERT INTO `gebruikers` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('082381', 'famgoossens@goossens.nl', 'Jan Goossens', 'Jan', 'Goossens', 'Grote Heistraat', '31', '2341BE', 'Rotterdam', 'Nederland', 'Gebruiker');
+INSERT INTO `gebruikers` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('052821', 'blijemiesje@gmail.com', 'MeisjeMiesje', 'Wies', 'Klarens', 'Vrijstraat', '28A', '3727MK', 'Etten-Leur', 'Nederlands', 'Gebruiker');
+
+/* INSERT data `gebruikers` */
+CALL `insert_pakketvolgnr` (05118987073542,"DHL","Next day",1);
+CALL `insert_pakketvolgnr` (05118987073543,"PostNL","Next day",2);
+CALL `insert_pakketvolgnr` (05118987073544,"DHL","Next day",3);
+CALL `insert_pakketvolgnr` (05118987073545,"DHL","Next day",4);
+CALL `insert_pakketvolgnr` (05118987073546,"DHL","Next day",5);
+CALL `insert_pakketvolgnr` (05118987073547,"PostNL","Next day",6);
+CALL `insert_pakketvolgnr` (05118987073548,"DHL","Next day",7);
+CALL `insert_pakketvolgnr` (05118987073549,"DHL","Next day",8);
+CALL `insert_pakketvolgnr` (05118987073551,"PostNL","Next day",9);
+CALL `insert_pakketvolgnr` (05118987073552,"DHL","Next day",10);
