f2471cbf10559ca5f644522356824c634d73c748
Renee-Avans <58734548+Renee-Avans@users.noreply.github.com>
2019-12-16 01:13:50 +0100

    Update Create-Stored Procedures.sql

--- SQL-GiftIt/Create-Stored Procedures.sql
+++ SQL-GiftIt/Create-Stored Procedures.sql
@@ -2,20 +2,20 @@
 /* Create Stored Procedure */
 DELIMITER //
 CREATE PROCEDURE `insert_pakketvolgnr` (
-	IN `par_TrackingNO` VARCHAR(40), 
-	IN `par_Vervoerder` VARCHAR(35), 
-	IN `par_ServiceLevel` VARCHAR(35), 
-	IN `par_OrderID` INT(6)) 
-BEGIN 
-	INSERT INTO `pakketvolgnr` ( 
-	`TrackingNO`, 
-	`Vervoerder`, 
-	`ServiceLevel`, 
-	`OrderID`) VALUES ( 
-	NULL, 
-	`par_TrackingNO`, 
-	`par_Vervoerder`, 
-	`par_ServiceLevel`, 
+	IN `par_TrackingNO` VARCHAR(40),
+	IN `par_Vervoerder` VARCHAR(35),
+	IN `par_ServiceLevel` VARCHAR(35),
+	IN `par_OrderID` INT(10))
+BEGIN
+	INSERT INTO `pakketvolgnr` (
+	`TrackingNO`,
+	`Vervoerder`,
+	`ServiceLevel`,
+	`OrderID`) VALUES (
+	NULL,
+	`par_TrackingNO`,
+	`par_Vervoerder`,
+	`par_ServiceLevel`,
 	`par_OrderID`);
 END;
 //
@@ -27,15 +27,15 @@ CALL `insert_pakketvolgnr` (TrackingNO,Vervoerder,ServiceLevel,OrderID)
 /* Stored Prcedure Vervoerder aanmaken */
 DELIMITER //
 CREATE PROCEDURE `insertVervoerder`
-  (IN `par_vervoerder` VARCHAR(35),IN `par_ServiceLevel` VARCHAR(35), OUT `resultaat1` VARCHAR(35),OUT `resultaat2` VARCHAR(35),OUT `resultaat3` VARCHAR(80),OUT `resultaat4` VARCHAR(50))
+	(IN `par_vervoerder` VARCHAR(35),IN `par_ServiceLevel` VARCHAR(35), OUT `resultaat1` VARCHAR(35),OUT `resultaat2` VARCHAR(35),OUT `resultaat3` VARCHAR(80),OUT `resultaat4` VARCHAR(50))
 BEGIN
-  INSERT INTO `giftit`.`vervoerder`(`Vervoerder`,`ServiceLevel`) 
-  VALUES(`par_vervoerder`, `par_ServiceLevel`);
-  Select `Vervoerder`,`ServiceLevel`,`Omschrijving`,`LinkVervoerder`
-  into `resultaat1`,`resultaat2`,`resultaat3`,`resultaat4`
-  from `giftit`.`vervoerder`
-  where `Vervoerder`= `par_vervoerder`
-  and `ServiceLevel` = `par_ServiceLevel`;
+	INSERT INTO `giftit`.`vervoerder`(`Vervoerder`,`ServiceLevel`)
+	VALUES(`par_vervoerder`, `par_ServiceLevel`);
+	Select `Vervoerder`,`ServiceLevel`,`Omschrijving`,`LinkVervoerder`
+	into `resultaat1`,`resultaat2`,`resultaat3`,`resultaat4`
+	from `giftit`.`vervoerder`
+	where `Vervoerder`= `par_vervoerder`
+	and `ServiceLevel` = `par_ServiceLevel`;
 END;
 //
 
@@ -52,7 +52,7 @@ FOR EACH ROW
 
 BEGIN
 	INSERT INTO `giftit`.`transactie` VALUES (
-        null, new.OrderID, new.AdvID, new.GebruikersID, new.StatusOrder, new.StatusOrder);
+		null, new.OrderID, new.AdvID, new.GebruikersID, new.StatusOrder, new.StatusOrder);
 END
 //
 
@@ -64,6 +64,6 @@ FOR EACH ROW
 
 BEGIN
 	INSERT INTO `giftit`.`transactie` VALUES (
-        null, new.OrderID, new.AdvID, new.GebruikersID, new.StatusOrder, new.StatusOrder);
+		null, new.OrderID, new.AdvID, new.GebruikersID, new.StatusOrder, new.StatusOrder);
 END
 //
