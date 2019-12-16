91cb4c4c93698844b28e6386efad9d47066a3cab
Renee-Avans <58734548+Renee-Avans@users.noreply.github.com>
2019-12-16 01:19:24 +0100

    Update INSERT-Data.sql

--- SQL-GiftIt/INSERT-Data.sql
+++ SQL-GiftIt/INSERT-Data.sql
@@ -1,20 +1,38 @@
 /* INSERT data `gebruikers` */
-INSERT INTO `gebruiker` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('124913', 'jan.jansen@gmail.com', 'Jantje', 'Jan', 'Jansen', 'Kerkstraat', '21', '1234DE', 'Schiedam', 'Nederland', 'Gebruiker');
-INSERT INTO `gebruiker` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('232143', 'familiedebie@hotmail.com', 'Jo de Bie', 'Jo', 'de Bie', 'Markt', '6', '3221AA', 'Amsterdam', 'Nederland', 'Gebruiker');
-INSERT INTO `gebruiker` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('082381', 'famgoossens@goossens.nl', 'Jan Goossens', 'Jan', 'Goossens', 'Grote Heistraat', '31', '2341BE', 'Rotterdam', 'Nederland', 'Gebruiker');
-INSERT INTO `gebruiker` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('052821', 'blijemiesje@gmail.com', 'MeisjeMiesje', 'Wies', 'Klarens', 'Vrijstraat', '28A', '3727MK', 'Etten-Leur', 'Nederlands', 'Gebruiker');
+INSERT INTO `gebruiker` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('1247911343', 'jan.jansen@gmail.com', 'Jantje', 'Jan', 'Jansen', 'Kerkstraat', '21', '1234DE', 'Schiedam', 'Nederland', 'Gebruiker');
+INSERT INTO `gebruiker` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('2321214543', 'familiedebie@hotmail.com', 'Jo de Bie', 'Jo', 'de Bie', 'Markt', '6', '3221AA', 'Amsterdam', 'Nederland', 'Gebruiker');
+INSERT INTO `gebruiker` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('0823315681', 'famgoossens@goossens.nl', 'Jan Goossens', 'Jan', 'Goossens', 'Grote Heistraat', '31', '2341BE', 'Rotterdam', 'Nederland', 'Gebruiker');
+INSERT INTO `gebruiker` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('0521279821', 'blijemiesje@gmail.com', 'MeisjeMiesje', 'Wies', 'Klarens', 'Vrijstraat', '28A', '3727MK', 'Etten-Leur', 'Nederlands', 'Gebruiker');
+INSERT INTO `gebruiker` (`GebruikersID`, `Email`, `GebruikersNaam`, `Voornaam`, `Achternaam`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`, `Rol`) VALUES ('1553783873', 'gabriellajagers@hetnet.nl', 'De jager', 'Gabriella', 'Jagers', 'Zellesacker', '138', '6546HH', 'Nijmegen', 'Nederlands', 'Gebruiker');
 
 /* INSERT data `categorie` */
-INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (1, 'Huis en Inrichting', NULL);
-INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (NULL, 'Antiek en Kunst', NULL);
-INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (NULL, 'Badkamer', 1);
-INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (NULL, 'Woonkamer', 1);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (1, 'Huis en Inrichting', 1);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (2, 'Woonkamer', 1);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (3, 'Badkamer', 1);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (4, 'Slaapkamer', 1);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (5, 'Lampen', 1);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (6, 'Woonaccessoires', 1);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (7, 'Kunst', 7);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (8, 'Kleding', 8);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (9, 'Mannenkleding', 8);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (10, 'Vrouwenkleding', 8);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (11, 'Kinderkleding', 8);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (12, 'Voor de baby', 12);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (13, 'Babyspeelgoed', 12);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (14, 'Baby kleding', 12);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (15, 'Boeken', 15);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (16, 'Hobby en vrije tijd', 16);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (17, 'Sieraden', 17);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (18, 'Electronica', 18);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (19, 'Games & consoles', 18);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (20, 'Muziekinstrumenten', 20);
+INSERT INTO `categorie` (`CategorieID`, `Naam`, `SubCategorieVan`) VALUES (21, 'Verzamelen', 21);
 
 /* INSERT `booking` */
-INSERT INTO `booking` (`BookingID`, `Vervoerder`, `ServiceLevel`, `AdvID`, `GebruikerID`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`) VALUES (NULL, 'TNT', 'EXPRESS-PLUS', 1, 232143, 'Markt', '6', '3221AA', 'Amsterdam', 'Nederland');
-INSERT INTO `booking` (`BookingID`, `Vervoerder`, `ServiceLevel`, `AdvID`, `GebruikerID`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`) VALUES (NULL, 'TNT', 'EXPRESS-PLUS', 2, 232143, 'Markt', '6', '3221AA', 'Amsterdam', 'Nederland');
-INSERT INTO `booking` (`BookingID`, `Vervoerder`, `ServiceLevel`, `AdvID`, `GebruikerID`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`) VALUES (NULL, 'TNT', 'EXPRESS-PLUS', 3, 052821, 'Vrijstraat', '28A', '3727MK', 'Etten-Leur', 'Nederlands');
-INSERT INTO `booking` (`BookingID`, `Vervoerder`, `ServiceLevel`, `AdvID`, `GebruikerID`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`) VALUES (NULL, 'TNT', 'EXPRESS-PLUS', 4, 124913, 'Kerkstraat', '21', '1234DE', 'Schiedam', 'Nederland');
+INSERT INTO `booking` (`BookingID`, `Vervoerder`, `ServiceLevel`, `AdvID`, `GebruikersID`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`) VALUES (NULL, 'TNT', 'EXPRESS-PLUS', 1, 232143, 'Markt', '6', '3221AA', 'Amsterdam', 'Nederland');
+INSERT INTO `booking` (`BookingID`, `Vervoerder`, `ServiceLevel`, `AdvID`, `GebruikersID`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`) VALUES (NULL, 'TNT', 'EXPRESS-PLUS', 2, 232143, 'Markt', '6', '3221AA', 'Amsterdam', 'Nederland');
+INSERT INTO `booking` (`BookingID`, `Vervoerder`, `ServiceLevel`, `AdvID`, `GebruikersID`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`) VALUES (NULL, 'TNT', 'EXPRESS-PLUS', 3, 052821, 'Vrijstraat', '28A', '3727MK', 'Etten-Leur', 'Nederlands');
+INSERT INTO `booking` (`BookingID`, `Vervoerder`, `ServiceLevel`, `AdvID`, `GebruikersID`, `Straat`, `Huisnummer`, `Postcode`, `Stad`, `Land`) VALUES (NULL, 'TNT', 'EXPRESS-PLUS', 4, 124913, 'Kerkstraat', '21', '1234DE', 'Schiedam', 'Nederland');
 
 /* INSERT data `pakketvolgnr` */
 CALL `insert_pakketvolgnr` (05118987073542,"DHL","Next day",1);
