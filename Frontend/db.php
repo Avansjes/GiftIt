6b5d7cd48c85b12d03fa4e5da0ff98efb8d16571
Renee-Avans <58734548+Renee-Avans@users.noreply.github.com>
2019-12-30 20:05:49 +0100

    Create db.php

---
+++ Frontend/db.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("localhost","root","","giftit");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Er kon geen verbinding gemaakt worden met MySQL: " . mysqli_connect_error();
+  }
+?>
