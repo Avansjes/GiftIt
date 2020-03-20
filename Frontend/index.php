a0079269ca5e4b1ddef9773448d85bbc09d228da
Bas Jongbloed <basjongbloed@gmail.com>
2020-03-20 23:50:27 +0100

    Lijst met dingen.

--- Frontend/index.php
+++ Frontend/index.php
@@ -1,6 +1,7 @@
 <?php
 //include auth.php file on all secure pages
 include("auth.php");
+require('db.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,6 +17,20 @@ include("auth.php");
 <div class="form login">
 <p>Welkom <?php echo $_SESSION['GebruikersNaam']; ?>!</p>
 <p>Je bent nu ingelogd.</p>
+<?php
+	 $query = "SELECT * FROM `advertentie`";
+
+	 $result = mysqli_query($con,$query) or die(mysql_error());
+
+	while($row = $result->fetch_assoc()) { ?>
+
+	<br>
+	<div class="feedbody">
+		<div class="title"><?php echo $row["Titel"]; ?></div><br>
+		<div class="feed-data"><span><?php echo $row["Inhoud"]; ?></span></div><br><br>
+	</div>
+	<?php }
+?>
 <a href="logout.php">Logout</a>
 </div>
 <input type="submit" class="giftbtn" name="gift" value="Plaats een gift!">
